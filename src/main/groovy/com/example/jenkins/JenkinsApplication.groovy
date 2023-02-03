5cc02c271462e1af3db53fa350775c9b2e3f7799
ksml21 <anthonymontoyasantos@gmail.com>
2023-02-03 12:49:17 -0500

    firste commit

---
+++ src/main/groovy/com/example/jenkins/JenkinsApplication.groovy
@@ -0,0 +1,13 @@
+package com.example.jenkins
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class JenkinsApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(JenkinsApplication, args)
+    }
+
+}
